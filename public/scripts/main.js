d1b3558f069b7a2f97484371cfbf88d038adf8ec
todiros <todiros@gmail.com>
2017-12-27 20:14:57 -0500

    deleted main.bundle.js and scripts/main.js

--- public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// import mobileCollapse from 'scripts/partials/mobileCollapseNav';
-const mobileCollapse = () => {
-  $(".button-collapse").sideNav();
-};
-
-$(document).ready(() => {
-    mobileCollapse();
-});
