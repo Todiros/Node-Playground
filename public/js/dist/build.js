865f97f3e709cea78aa128f7fd433bf8ae0a7477
todiros <todiros@gmail.com>
2017-12-27 20:11:49 -0500

    main.bundle.js changed to build.js

---
+++ public/js/dist/build.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _test = require('imports/test');
+
+var _test2 = _interopRequireDefault(_test);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// const mobileCollapse = () => {
+//   $(".button-collapse").sideNav();
+// };
+
+$('body').click(function () {
+    (0, _test2.default)('Test!');
+});
