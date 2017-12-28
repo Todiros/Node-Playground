019932524354e03ebaa747fa96286cbfdee1e9b0
todiros <todiros@gmail.com>
2017-12-27 20:08:03 -0500

    script folder changed to js, test.js added, ES6 compiled code

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// import consoleLog from 'imports/test';
+
+// const mobileCollapse = () => {
+//   $(".button-collapse").sideNav();
+// };
+
+$('body').click(() => {
+    console.log('Test!');
+});
